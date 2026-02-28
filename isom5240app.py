6328714a60abb752aa48df82d2f14d45e69b1457
tonyho5689 <tonyho5689@users.noreply.github.com>
2026-02-28 16:47:37 +0800

    Update isom5240app.py

--- isom5240app.py
+++ isom5240app.py
@@ -1,3 +1,4 @@
+from streamlit as st
 from transformers import pipeline
 from PIL import Image
 
@@ -21,4 +22,5 @@ age_predictions = sorted(age_predictions, key=lambda x: x['score'], reverse=True
 print("Predicted Age Range:")
 print(f"Age range: {age_predictions[0]['label']}")
 
+st.write("Done")
 
