e30bd1af6fc7d80ab92170ddc525892d6ca7bb04
tonyho5689 <tonyho5689@users.noreply.github.com>
2026-02-28 15:09:35 +0800

    Update isom5240app.py

--- isom5240app.py
+++ isom5240app.py
@@ -1,24 +1,28 @@
 import streamlit as st
-from transformers import pipeline
 
-# Load the text classification model pipeline
-classifier = pipeline("text-classification",model='isom5240ust/bert-base-uncased-emotion')
+st.write("5240")
 
-# Streamlit application title
-st.title("Text Classification for you")
-st.write("Classification for 6 emotions: sadness, joy, love, anger, fear, surprise")
+# import streamlit as st
+# from transformers import pipeline
 
-# Text input for user to enter the text to classify
-text = st.text_area("Enter the text to classify", "")
+# # Load the text classification model pipeline
+# classifier = pipeline("text-classification",model='isom5240ust/bert-base-uncased-emotion')
 
-# Perform text classification when the user clicks the "Classify" button
-if st.button("Classify"):
-    # Perform text classification on the input text
-    result = classifier(text)[0]
+# # Streamlit application title
+# st.title("Text Classification for you")
+# st.write("Classification for 6 emotions: sadness, joy, love, anger, fear, surprise")
 
-    # Display the classification result
-    st.write("Text:", text)
-    st.write("Label:", result['score'])
-    st.write("Score:", result['label'])
+# # Text input for user to enter the text to classify
+# text = st.text_area("Enter the text to classify", "")
+
+# # Perform text classification when the user clicks the "Classify" button
+# if st.button("Classify"):
+#     # Perform text classification on the input text
+#     result = classifier(text)[0]
+
+#     # Display the classification result
+#     st.write("Text:", text)
+#     st.write("Label:", result['score'])
+#     st.write("Score:", result['label'])
 
 
